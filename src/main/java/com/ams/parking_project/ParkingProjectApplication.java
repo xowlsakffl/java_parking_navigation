fbb32b959387910e431abde096bc2c5505d19a5d
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-07-30 00:22:08 +0900

    #1 초기 세팅

---
+++ src/main/java/com/ams/parking_project/ParkingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ams.parking_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ams/parking_project/ParkingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.parking_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
